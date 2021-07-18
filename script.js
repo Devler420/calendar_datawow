570acaa65b7f02a0e54ca0ae026dbd688976133a
Panupong Boom Utvichai <boompanupong@gmail.com>
2021-07-18 23:50:27 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const date = new Date();
+
+//getMonth() index number starts from 0
+const month = date.getMonth();
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+console.log(month);
